580c84068e382981e86b740a992e5f48a02bb7a4
JohanUwUTzul <johanisraeltzulcoyoy@gmail.com>
2024-08-21 15:25:05 -0600

    Add files via upload

---
+++ Implementar_capa_de_persistencia_con_Spring_Data_MongoDB/mongodb-demo/src/main/java/com/example/mongodb_demo/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.mongodb_demo.service;
+
+import com.example.mongodb_demo.Model.User;
+import com.example.mongodb_demo.Reposity.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
